24ebeaccfdcbbd4f389f777b6dafc2782a512121
JiLaRiAtIoN <117482776+JiLaRiAtIoN@users.noreply.github.com>
2023-12-17 15:24:15 +0300

    JRTB-1: added repository layer (#21)
    * JRTB-1: added repository layer

--- src/main/java/com/github/javarushcommunity/jrtb/command/CommandName.java
+++ src/main/java/com/github/javarushcommunity/jrtb/command/CommandName.java
@@ -8,7 +8,8 @@ public enum CommandName {
     START("/start"),
     STOP("/stop"),
     HELP("/help"),
-    NO("/no_com");
+    NO("/no_com"),
+    STAT("/stat");
 
     private final String commandName;
 
